8454d624739ef3bb607423c317f66cd94722d011
Alejandro <alejandrocaloto1@gmail.com>
2026-02-17 10:43:28 +0100

    Mejora del stock para que ahora un stock del producto de cada plataforma que tenga

--- ArcadiaGames/app/src/main/java/com/example/prueba1integrador/Juego.kt
+++ ArcadiaGames/app/src/main/java/com/example/prueba1integrador/Juego.kt
@@ -1,22 +1,27 @@
 package com.example.prueba1integrador
 
+import java.io.Serializable
+
 data class Juego(
     var id: String = "",
     val nombre: String = "",
     val descripcion: String = "",
-    val imagenResId: Int = 0, // Campo nuevo para recursos locales
+    val imagenResId: Int = 0,
     val imagenUrl: String = "",
     val precio: String = "",
     val tags: List<String> = emptyList(),
     val categoria: String = "",
     val plataforma: String = "",
-    val stock: Int = 0
-) : java.io.Serializable
+    val stock: Int = 0,
+    val detalle_stock: Map<String, Int>? = null
+) : Serializable
 
-// Clase de datos auxiliar para manejar Juego + Cantidad
 data class ItemInventario(
     val juego: Juego,
     val cantidad: Int,
-    val idsAgrupados: List<String> // Guardamos todos los IDs que forman este grupo
-)
-
+    val idsAgrupados: List<String>,
+    val ps: Int = 0,
+    val xb: Int = 0,
+    val ni: Int = 0,
+    val pc: Int = 0
+) : Serializable
